2f833bc21ac0bbb8f174cb4f480cb62bc22c99aa
sajjadalijaved <sajjadali11755@gmail.com>
2024-02-05 19:51:39 +0500

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:glass_morphism/login_screen.dart';
-import 'package:expansion_tile_card/expansion_tile_card.dart';
-// ignore_for_file: library_private_types_in_public_api
 
 void main() {
   runApp(const MyApp());
@@ -17,59 +15,5 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(),
         home: const LoginScreen());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<ExpansionTileCardState> cardA =
-      GlobalKey<ExpansionTileCardState>();
-  final GlobalKey<ExpansionTileCardState> cardB =
-      GlobalKey<ExpansionTileCardState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      body: ListView(children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-          child: ExpansionTileCard(
-            key: cardB,
-            leading: const CircleAvatar(child: Text('S')),
-            title: const Text('Sajjad Ali'),
-            subtitle: const Text('It has the Expanded Logo.'),
-            children: <Widget>[
-              const Divider(
-                thickness: 1.0,
-                height: 1.0,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 8.0,
-                    ),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset('assets/boy.jpg'))),
-              ),
-            ],
-          ),
-        ),
-      ]),
-    );
   }
 }
